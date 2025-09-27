f47b2ba306156ae55ce8673e6dd83b13b9b8dbbd
Jonas Fackelmeyer <jonas@fackelmeyer.com>
2025-09-27 12:10:12 +0200

    Fixed java.net.SocketException: Connection reset

--- common/src/main/java/com/vexsoftware/votifier/VoteHandler.java
+++ common/src/main/java/com/vexsoftware/votifier/VoteHandler.java
@@ -2,7 +2,6 @@ package com.vexsoftware.votifier;
 
 import com.vexsoftware.votifier.model.Vote;
 import com.vexsoftware.votifier.net.VotifierSession;
-import io.netty.channel.Channel;
 
 public interface VoteHandler {
 
@@ -10,5 +9,10 @@ public interface VoteHandler {
 
     default void onError(Throwable throwable, boolean voteAlreadyCompleted, String remoteAddress) {
         throw new RuntimeException("Unimplemented onError handler");
+    }
+
+    default void onConnectionReset(String remoteAddress, boolean voteAlreadyCompleted) {
+        // Log connection reset at INFO level since it's a normal network event
+        System.out.println("[Votifier] Connection reset from " + remoteAddress + (voteAlreadyCompleted ? " (vote processed)" : " (vote incomplete)"));
     }
 }
