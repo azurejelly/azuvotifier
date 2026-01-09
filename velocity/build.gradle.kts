f430721d173054abed185225de7dcd26a565a8a2
azurejelly <178000437+azurejelly@users.noreply.github.com>
2026-01-09 13:51:10 -0500

    fix(velocity): update Velocity dependency

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 repositories {
-    maven("https://nexus.velocitypowered.com/repository/maven-public/")
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
